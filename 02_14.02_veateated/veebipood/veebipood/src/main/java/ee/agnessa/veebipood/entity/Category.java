42099abb64c07d690c69600dc2e8bc1b317d88b1
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-02-14 15:26:41 +0200

    14_02 veateated, kategooriad, entity

---
+++ 02_14.02_veateated/veebipood/veebipood/src/main/java/ee/agnessa/veebipood/entity/Category.java
@@ -0,0 +1,25 @@
+package ee.agnessa.veebipood.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//@Table(name = "kategooria")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    // @ColumnDefault("false")
+    // @ColumnDefault(0)
+    private boolean active;
+}
---
+++ 02_14.02_veateated/veebipood/veebipood/src/main/java/ee/agnessa/veebipood/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package ee.agnessa.veebipood.repository;
+
+import ee.agnessa.veebipood.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
